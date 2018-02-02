8c443f1e2c686370eb5bb58f95109700668d60b8
Wolfgang Mau <wolfgang@moia.io>
2018-02-02 06:02:38 +0100

    build all

--- build.sh
+++ build.sh
@@ -3,18 +3,18 @@
 rm -rf deploy/*
 
 echo "Linux"
-#docker pull therecipe/qt:linux
+docker pull therecipe/qt:linux
 qtdeploy -docker build linux
 zip -r deploy/chamgo-qt-linux.zip deploy/linux
 
 echo "windows64"
-#docker pull therecipe/qt:windows_64_static
+docker pull therecipe/qt:windows_64_static
 qtdeploy -docker build windows_64_static
 mv deploy/windows deploy/windows64
 zip -r deploy/chamgo-qt-win64.zip deploy/windows64
 
 echo "windows32"
-#docker pull therecipe/qt:windows_32_static
+docker pull therecipe/qt:windows_32_static
 qtdeploy -docker build windows_32_static
 mv deploy/windows deploy/windows32
 zip -r deploy/chamgo-qt-win32.zip deploy/windows32
